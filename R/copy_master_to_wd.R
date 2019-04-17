ae14957301609127ebc80e36835c7d74c4173c8a
andybeet <22455149+andybeet@users.noreply.github.com>
2019-04-17 12:31:50 -0400

    copy_master_to_wd.r - automatically copies template xlsx file from installation directory to users working directory

---
+++ R/copy_master_to_wd.R
@@ -0,0 +1,30 @@
+#'Copy Master_Template.xlsx to computer
+#'
+#'The Master_Template.xlsx file that is bundled with the \code{inportXML} package gets installed (by default) with your package.
+#'This is often in a location the typical user never looks. \code{copy_master_to_wd} allows the user to copy this file automatically
+#'without having to know where it was installed.
+#'
+#'@examples
+#'#'simply run the function to copy the file to your working directory
+#'
+#'@export
+
+copy_master_to_wd <- function() {
+
+  fileName <- "Master_Template.xlsx"
+  outPath <- paste0(getwd(),"/",fileName)
+  numPaths <- length(.libPaths())
+
+  for (ipath in 1:numPaths) {
+    inPath <- paste0(.libPaths()[ipath],"/inportXML/",fileName)
+    result <- file.copy(from = inPath, to = outPath, overwrite = FALSE)
+    if (result == TRUE) {
+      cat("The file ",fileName," was sucessfully copied to your working directory")
+      return()
+    }
+  }
+
+  warning("The file (Master_Template.xlsx) FAILED TO COPY. Maybe the file already exists in you working directory!")
+
+
+}
