bf27afea9396e7a49f693d6a5f2f4f0debb384b4
andybeet <22455149+andybeet@users.noreply.github.com>
2019-09-05 15:35:01 -0400

    package description r file

---
+++ R/InportXML.R
@@ -0,0 +1,15 @@
+#' InportXML: xml creation for Inport
+#'
+#' Tools to aid in creation of xml files for direct upload into Inport (\url{https://inport.nmfs.noaa.gov/inport/})
+#'
+#'\itemize{
+#'\item User must complete the Master xml template
+#'\item This is then converted into an xml file(s) for user to upload into Inport
+#'}
+#'
+#'To learn more about using \code{InportXML}, start with the vignette: \code{browseVignettes(package="InportXML")} or click the index below
+#'
+#'
+#' @docType package
+#' @name InportXML
+NULL
